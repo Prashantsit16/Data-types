1d64bc4af8362df43fa7ae45d64055fb420b1f90
Prashantsit16 <prashant.bajpai.btech2023@sitpune.edu.in>
2024-08-10 17:26:20 +0530

    Add files via upload

---
+++ size_of_datatypes.cpp
@@ -0,0 +1,18 @@
+//Prashant bajpai
+//23070123102
+//size of primitive data types
+#include<iostream>
+using namespace std;
+int main()
+{
+    //size of helps us to determine the size of data types in bytes
+    cout<<"size of integer is "<<sizeof(int)<<endl;
+    cout<<"size of float is "<<sizeof(float)<<endl;
+    cout<<"size of double is "<<sizeof(double)<<endl;
+    cout<<"size of character is "<<sizeof(char)<<endl;
+    cout<<"size of string is "<<sizeof(string)<<endl;
+    cout<<"size of unsigned integer is "<<sizeof(unsigned int)<<endl;
+    cout<<"size of long is "<<sizeof(long)<<endl;
+    cout<<"size of boolean is "<<sizeof(bool)<<endl;
+    return 0;
+}
---
+++ storageclass.cpp
@@ -0,0 +1,19 @@
+//Prashant bajpai
+//23070123102
+//Static storage class
+#include<iostream>
+using namespace std;
+void staticExample(){
+    //int z=0;
+    static int z=0;
+    z++;
+    cout<< "The value of z is: "<<z<<endl;
+}
+int main()
+
+{
+    staticExample();
+    staticExample();
+    staticExample();   
+    return 0 ; 
+}
